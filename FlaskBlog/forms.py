45fcee8267be09f11e697dd6a6a2724ac6a258c1
Joshua Tan <joshuatze97@gmail.com>
2021-01-22 01:23:36 +0800

    added some stuff to forms

--- FlaskBlog/forms.py
+++ FlaskBlog/forms.py
@@ -1,4 +1,6 @@
 from flask_wtf import FlaskForm
+from wtforms import StringField
+from wtforms.validators import DataRequired, Length
 
 class Registration(FlaskForm):
-    
+    username = StringField('Username', validators=[DataRequired(), Length(min=2,max=20)])
