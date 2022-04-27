f90a83e904e0534d4b1641e234a1c1f507d96cd7
Joshua Tan <joshuatze97@gmail.com>
2022-04-27 16:31:27 +0800

    Added some precommit hooks

--- test.py
+++ test.py
@@ -15,17 +15,17 @@ from tensorflow.keras.preprocessing.image import ImageDataGenerator
 run = Run.get_context()
 
 # Define source model name if fine tuning a model
-SRC_MODEL_NAME = 'retcam_stage1'
-DEST_MODEL_NAME = f'retcam_neo_2022_eff0---{utils.getStandardTime()}'
+SRC_MODEL_NAME = "retcam_stage1"
+DEST_MODEL_NAME = f"retcam_neo_2022_eff0---{utils.getStandardTime()}"
 
-FIRST_EPOCHS = 1   
-SECOND_EPOCHS = 10 
-LR1 = 0.0088        #0.0088, 0.088, 0.00088, 0.02, 0.01
-LR2 = 0.0001        #0.00037, 0.0037, 0.000037, 0.002,   0.00088, reduce by a factor of 10
-DR1 = 0.235         # dropout rate 0.435
-DR2 = 0.348         # 0.648
+FIRST_EPOCHS = 1
+SECOND_EPOCHS = 10
+LR1 = 0.0088  # 0.0088, 0.088, 0.00088, 0.02, 0.01
+LR2 = 0.0001  # 0.00037, 0.0037, 0.000037, 0.002,   0.00088, reduce by a factor of 10
+DR1 = 0.235  # dropout rate 0.435
+DR2 = 0.348  # 0.648
 DENSE1 = 235
-INPUT_SHAPE = (168, 224, 3)    #b0(168,224,3), b1(180,240,3)
+INPUT_SHAPE = (168, 224, 3)  # b0(168,224,3), b1(180,240,3)
 BATCH_SIZE = 64
 N_TRAIN_STEPS = 5
 
@@ -38,7 +38,7 @@ model_parameters = {
     "DR1": DR1,
     "DR2": DR2,
     "DENSE1": DENSE1,
-    "INPUT_SHAPE": INPUT_SHAPE
+    "INPUT_SHAPE": INPUT_SHAPE,
 }
 
 # Csvs names (combined retcam + neo)
@@ -53,7 +53,9 @@ targetCol = "hasROP"
 
 
 def get_class_weight(labels):
-    class_weight_arr = cw.compute_class_weight('balanced', classes=np.unique(labels), y=labels)
+    class_weight_arr = cw.compute_class_weight(
+        "balanced", classes=np.unique(labels), y=labels
+    )
     class_weights = {}
     for i in range(len(class_weight_arr)):
         class_weights[i] = class_weight_arr[i]
@@ -63,8 +65,10 @@ def get_class_weight(labels):
 def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument("--dir_path", type=str, help="path to training data")
-    parser.add_argument('--checkpoint_path', type=str)
-    parser.add_argument("--output_blob_path", type=str, help="path to output, directly to blob")
+    parser.add_argument("--checkpoint_path", type=str)
+    parser.add_argument(
+        "--output_blob_path", type=str, help="path to output, directly to blob"
+    )
     args = parser.parse_args()
     return args
 
@@ -75,50 +79,52 @@ def main(args):
 
     dir_path = args.dir_path
     output_blob_path = args.output_blob_path
-    pre_train_weights_path = os.path.join(dir_path,"models","noisy.student.notop-b0.h5")
+    pre_train_weights_path = os.path.join(
+        dir_path, "models", "noisy.student.notop-b0.h5"
+    )
 
-    print(f"Is pre_train_weights_path valid? : {os.path.exists(pre_train_weights_path)}")
+    print(
+        f"Is pre_train_weights_path valid? : {os.path.exists(pre_train_weights_path)}"
+    )
     print(f"Pre_train_weights_path: {pre_train_weights_path}")
-    
+
     print("dir_path", "=" * 50)
     print(dir_path)
     print("output_blob_path", "=" * 50)
     print(output_blob_path)
-    
-    # provide path to csvs for training 
+
+    # provide path to csvs for training
     csvDir = os.path.join(dir_path, "binary_rop_classifier", "csvs")
     train_df = pd.read_csv(os.path.join(csvDir, train_csv_path))
     val_df = pd.read_csv(os.path.join(csvDir, val_csv_path))
     train_df = train_df[train_df["isExist"] == True]
     val_df = val_df[val_df["isExist"] == True]
-    
-    train_df[srcPathCol] = train_df[srcPathCol].apply(lambda x: os.path.join(dir_path, x))
+
+    train_df[srcPathCol] = train_df[srcPathCol].apply(
+        lambda x: os.path.join(dir_path, x)
+    )
     val_df[srcPathCol] = val_df[srcPathCol].apply(lambda x: os.path.join(dir_path, x))
-    
 
     # Assigning labels
-    train_df['hasROP'].loc[train_df['hasROP'] == True] = pos_label   #"Have ROP"
-    train_df['hasROP'].loc[train_df['hasROP'] == False] = neg_label  #"No ROP"
-    val_df['hasROP'].loc[val_df['hasROP'] == True] = pos_label       #"Have ROP" 
-    val_df['hasROP'].loc[val_df['hasROP'] == False] = neg_label      #"No ROP"
+    train_df["hasROP"].loc[train_df["hasROP"] == True] = pos_label  # "Have ROP"
+    train_df["hasROP"].loc[train_df["hasROP"] == False] = neg_label  # "No ROP"
+    val_df["hasROP"].loc[val_df["hasROP"] == True] = pos_label  # "Have ROP"
+    val_df["hasROP"].loc[val_df["hasROP"] == False] = neg_label  # "No ROP"
 
     # sanity check if path is valid
     print("train_df: ")
-    for idx,row in train_df.iterrows():
+    for idx, row in train_df.iterrows():
         if idx >= 10:
             break
         print(row[srcPathCol])
         print(os.path.exists(row[srcPathCol]))
-    
 
     # Using ImagedataGenerators to form batches
     train_datagen = ImageDataGenerator(
-        preprocessing_function=train_utils.preprocess_augment_fn   
+        preprocessing_function=train_utils.preprocess_augment_fn
     )
 
-    val_datagen = ImageDataGenerator(
-        preprocessing_function=train_utils.preprocess_fn
-    )
+    val_datagen = ImageDataGenerator(preprocessing_function=train_utils.preprocess_fn)
 
     train_gen = train_datagen.flow_from_dataframe(
         train_df,
@@ -126,9 +132,9 @@ def main(args):
         y_col=targetCol,
         target_size=INPUT_SHAPE[:2],
         batch_size=BATCH_SIZE,
-        class_mode='binary',
+        class_mode="binary",
         shuffle=True,
-        validate_filenames=False
+        validate_filenames=False,
     )
 
     test_gen = val_datagen.flow_from_dataframe(
@@ -137,22 +143,7 @@ def main(args):
         y_col=targetCol,
         target_size=INPUT_SHAPE[:2],
         batch_size=BATCH_SIZE,
-        class_mode='binary',
+        class_mode="binary",
         shuffle=True,
-        validate_filenames=False
-<<<<<<< HEAD
+        validate_filenames=False,
     )
-    
-print('hola amiguss')
-print('pull pull pull request baby')
-print('damn')
-
-
-def subtract(a,b):
-    print('hello world ')
-    print('hello world 2.0')
-    print('hello world 3.0')
-    return a-b 
-=======
-    )
->>>>>>> parent of 0174f68 (Update test.py)
