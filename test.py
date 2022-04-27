188797fdb43ec5d60e2f854da849f09f75115538
Joshua Tan <joshuatze97@gmail.com>
2022-04-27 11:16:28 +0800

    added docstrings to subtract

--- test.py
+++ test.py
@@ -148,5 +148,13 @@ print('damn')
 
 
 def subtract(a,b):
-     
+    """_summary_
+
+    Args:
+        a (_type_): _description_
+        b (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """
     return a-b 
